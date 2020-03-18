78e8029d2cc690899a7d80d246b0ebc4f2791b44
BAHOHCEHC <bahohcehc@gmail.com>
2020-03-19 00:35:10 +0200

    todo app with mongo

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: 'mongodb+srv://bahohcehc:bahohcehc@node-todo-istho.mongodb.net/todos'
+};
--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-console.log('********');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const exphbars = require('express-handlebars');
+
+// const Handlebars = require('handlebars');
+// const {
+//   allowInsecurePrototypeAccess
+// } = require('@handlebars/allow-prototype-access');
+
+const keys = require('./config/keys');
+const todosRoutes = require('./routes/todos');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const hbs = exphbars.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+  allowProtoMethodsByDefault: true,
+  allowProtoPropertiesByDefault: true
+});
+
+app.engine('hbs', hbs.engine);
+// app.engine(
+//   'hbs',
+//   exphbars({
+//     handlebars: allowInsecurePrototypeAccess(Handlebars)
+//   })
+// );
+
+
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(todosRoutes);
+
+
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() =>
+    console.log('***************MongoDB connected!!!!!!!!!***************')
+  )
+  .catch(error =>
+    console.log('***************ERROR!!!!!!!!!***************', error)
+  );
+
+app.listen(PORT, () => {
+  console.log('***************SERVER WORK ON PORT***************', PORT);
+});
